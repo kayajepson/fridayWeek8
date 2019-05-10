3520929177aece600c26e1d6d03afc328822827a
kaya <jepsonkaya@gmail.com>
2019-05-10 09:31:16 -0700

    trying to get mysql working? getting lots of errors

--- HairSalon.Solutions/HairSalon.Tests/ModelTests/StylistTests.cs
+++ HairSalon.Solutions/HairSalon.Tests/ModelTests/StylistTests.cs
@@ -20,172 +20,142 @@ namespace HairSalon.Tests
     }
 
     [TestMethod]
-      public void StylistConstructor_CreatesInstanceOfStylist_Stylist()
-      {
-        Stylist newStylist = new Stylist("test stylist");
-        Assert.AreEqual(typeof(Stylist), newStylist.GetType());
-      }
-      [TestMethod]
+    public void StylistConstructor_CreatesInstanceOfStylist_Stylist()
+    {
+      Stylist newStylist = new Stylist("test stylist");
+      Assert.AreEqual(typeof(Stylist), newStylist.GetType());
+    }
+
+    [TestMethod]
     public void GetName_ReturnsName_String()
     {
       //Arrange
-      string name = "Test Category";
-      Category newCategory = new Category(name);
+      string name = "test Stylist";
+      Stylist newStylist = new Stylist(name);
 
       //Act
-      string result = newCategory.GetName();
+      string result = newStylist.GetName();
 
       //Assert
       Assert.AreEqual(name, result);
     }
 
-    // [TestMethod]
-    // public void GetId_ReturnsCategoryId_Int()
-    // {
-    //   //Arrange
-    //   string name = "Test Category";
-    //   Category newCategory = new Category(name);
-    //
-    //   //Act
-    //   int result = newCategory.GetId();
-    //
-    //   //Assert
-    //   Assert.AreEqual(1, result);
-    // }
-    //
     [TestMethod]
-  public void GetAll_ReturnsAllCategoryObjects_CategoryList()
-  {
-    //Arrange
-    string name01 = "Work";
-    string name02 = "School";
-    Category newCategory1 = new Category(name01);
-    newCategory1.Save();
-    Category newCategory2 = new Category(name02);
-    newCategory2.Save();
-    List<Category> newList = new List<Category> { newCategory1, newCategory2 };
-
-    //Act
-    List<Category> result = Category.GetAll();
-
-    //Assert
-    CollectionAssert.AreEqual(newList, result);
-  }
-
-    [TestMethod]
-    public void Find_ReturnsCorrectCategory_Category()
+    public void GetAll_ReturnsAllStylistObjects_StylistList()
     {
       //Arrange
-      Category testCategory = new Category("household chores");
-      testCategory.Save();
+      string name01 = "suzie";
+      string name02 = "tammy";
+      Stylist newStylist1 = new Stylist(name01);
+      newStylist1.Save();
+      Stylist newStylist2 = new Stylist(name02);
+      newStylist2.Save();
+      List<Stylist> newList = new List<Stylist> { newStylist1, newStylist2 };
 
       //Act
-      Category foundCategory = Category.Find(testCategory.GetId());
+      List<Stylist> result = Stylist.GetAll();
 
       //Assert
-      Assert.AreEqual(testCategory, foundCategory);
+      CollectionAssert.AreEqual(newList, result);
     }
 
     [TestMethod]
-    public void GetItems_ReturnsEmptyItemList_ItemList()
+    public void Find_ReturnsCorrectStylist_Stylist()
     {
-        //Arrange
-        string name = "Work";
-        Category newCategory = new Category(name);
-        List<Item> newList = new List<Item> { };
+      //Arrange
+      Stylist testStylist = new Stylist("suzie");
+      testStylist.Save();
 
-        //Act
-        List<Item> result = newCategory.GetItems();
+      //Act
+      Stylist foundStylist = Stylist.Find(testStylist.GetId());
 
-        //Assert
-        CollectionAssert.AreEqual(newList, result);
+      //Assert
+      Assert.AreEqual(testStylist, foundStylist);
     }
 
     // [TestMethod]
-    // public void AddItem_AssociatesItemWithCategory_ItemList()
+    // public void GetItems_ReturnsEmptyItemList_ItemList()
     // {
     //   //Arrange
-    //   string description = "Walk the dog.";
-    //   Item newItem = new Item(description, 1);
-    //   List<Item> newList = new List<Item> { newItem };
-    //   string name = "Work";
-    //   Category newCategory = new Category(name);
-    //   newCategory.AddItem(newItem);
+    //   string name = "suzie";
+    //   Stylist newStylist = new Stylist(name);
+    //   List<Item> newList = new List<Item> { };
     //
     //   //Act
-    //   List<Item> result = newCategory.GetItems();
+    //   List<Item> result = newStylist.GetItems();
     //
     //   //Assert
     //   CollectionAssert.AreEqual(newList, result);
     // }
 
-    [TestMethod]
-    public void GetItems_RetrievesAllItemsWithCategory_ItemList()
-    {
-      //Arrange, Act
-      Category testCategory = new Category("household chores");
-      testCategory.Save();
-      Item firstItem = new Item ("Mow the lawn", testCategory.GetId());
-      firstItem.Save();
-      Item secondItem = new Item("Do the dishes", testCategory.GetId());
-      secondItem.Save();
-      List<Item>testItemList = new List<Item> {firstItem, secondItem};
-      List<Item>resultItemList = testCategory.GetItems();
-
-      //Assert
-      CollectionAssert.AreEqual(testItemList, resultItemList);
-    }
+    // [TestMethod]
+    // public void GetItems_RetrievesAllItemsWithStylist_ItemList()
+    // {
+    //   //Arrange, Act
+    //   Stylist testStylist = new Stylist("suzie");
+    //   testStylist.Save();
+    //   Item firstItem = new Item ("Mow the lawn", testStylist.GetId());
+    //   firstItem.Save();
+    //   Item secondItem = new Item("Do the dishes", testStylist.GetId());
+    //   secondItem.Save();
+    //   List<Item>testItemList = new List<Item> {firstItem, secondItem};
+    //   List<Item>resultItemList = testStylist.GetItems();
+    //
+    //   //Assert
+    //   CollectionAssert.AreEqual(testItemList, resultItemList);
+    // }
 
     [TestMethod]
     public void GetAll_CategoriesEmptyAtFirst_List()
     {
       //Arrange, Act
-      int result = Category.GetAll().Count;
+      int result = Stylist.GetAll().Count;
 
       //Assert
       Assert.AreEqual(0, result);
     }
+
     [TestMethod]
-    public void Equals_ReturnsTrueIfNamesAreTheSame_Category()
+    public void Equals_ReturnsTrueIfNamesAreTheSame_Stylist()
     {
       //Arrange, Act
-      Category firstCategory = new Category("Household chores");
-      Category secondCategory = new Category("Household chores");
+      Stylist firstStylist = new Stylist("suzie");
+      Stylist secondStylist = new Stylist("suzie");
 
       //Assert
-      Assert.AreEqual(firstCategory, secondCategory);
+      Assert.AreEqual(firstStylist, secondStylist);
     }
     [TestMethod]
-    public void Save_SavesCategoryToDatabase_CategoryList()
+    public void Save_SavesStylistToDatabase_StylistList()
     {
       //Arrange
-      Category testCategory = new Category("household chores");
-      testCategory.Save();
+      Stylist testStylist = new Stylist("suzie");
+      testStylist.Save();
 
       //Act
-      List<Category> result = Category.GetAll();
-      List<Category> testList = new List<Category>{testCategory};
+      List<Stylist> result = Stylist.GetAll();
+      List<Stylist> testList = new List<Stylist>{testStylist};
 
       //Assert
       CollectionAssert.AreEqual(testList, result);
     }
 
     [TestMethod]
-    public void Save_DatabaseAssignsIdToCategory_Id()
+    public void Save_DatabaseAssignsIdToStylist_Id()
     {
       //Arrange
-      Category testCategory = new Category("household chores");
-      testCategory.Save();
+      Stylist testStylist = new Stylist("suzie");
+      testStylist.Save();
 
       //Act
-      Category savedCategory = Category.GetAll()[0];
+      Stylist savedStylist = Stylist.GetAll()[0];
 
-      int result = savedCategory.GetId();
-      int testId = testCategory.GetId();
+      int result = savedStylist.GetId();
+      int testId = testStylist.GetId();
 
       //Assert
       Assert.AreEqual(testId, result);
     }
 
-    }
   }
+}
--- HairSalon.Solutions/HairSalon/Models/Stylist.cs
+++ HairSalon.Solutions/HairSalon/Models/Stylist.cs
@@ -61,6 +61,129 @@ namespace HairSalon.Models
       return allStylists;
     }
 
+    public static Stylist Find(int id)
+      {
+        MySqlConnection conn = DB.Connection();
+        conn.Open();
+        var cmd = conn.CreateCommand() as MySqlCommand;
+        cmd.CommandText = @"SELECT * FROM stylists WHERE id = (@searchId);";
+        MySqlParameter searchId = new MySqlParameter();
+        searchId.ParameterName = "@searchId";
+        searchId.Value = id;
+        cmd.Parameters.Add(searchId);
+        var rdr = cmd.ExecuteReader() as MySqlDataReader;
+        int StylistId = 0;
+        string StylistName = "";
+        while(rdr.Read())
+        {
+          StylistId = rdr.GetInt32(0);
+          StylistName = rdr.GetString(1);
+        }
+        Stylist newStylist = new Stylist(StylistName, StylistId);
+        conn.Close();
+        if (conn != null)
+        {
+          conn.Dispose();
+        }
+        return newStylist;
+      }
+
+      // public void AddItem(Item item)
+      // {
+      //   _items.Add(item);
+      // }
+
+      // public List<Item> GetItems()
+      // {
+      //   List<Item> allStylistItems = new List<Item>{};
+      //   MySqlConnection conn = DB.Connection();
+      //   conn.Open();
+      //   var cmd = conn.CreateCommand() as MySqlCommand;
+      //   cmd.CommandText = @"SELECT * FROM items WHERE stylist_id = @stylist_id;";
+      //   MySqlParameter stylistId = new MySqlParameter();
+      //   stylistId.ParameterName = "@stylist_id";
+      //   stylistId.Value = this._id;
+      //   cmd.Parameters.Add(stylistId);
+      //   var rdr = cmd.ExecuteReader() as MySqlDataReader;
+      //   while(rdr.Read())
+      //   {
+      //      int itemId = rdr.GetInt32(0);
+      //      string itemDescription = rdr.GetString(1);
+      //      int itemStylistId = rdr.GetInt32(2);
+      //      Item newItem = new Item(itemDescription, itemStylistId, itemId);
+      //      allStylistItems.Add(newItem);
+      //   }
+      //   conn.Close();
+      //   if (conn != null)
+      //   {
+      //     conn.Dispose();
+      //   }
+      //   return allStylistItems;
+      // }
+
+      public override bool Equals(System.Object otherStylist)
+    {
+      if (!(otherStylist is Stylist))
+      {
+        return false;
+      }
+      else
+      {
+        Stylist newStylist = (Stylist) otherStylist;
+        bool idEquality = this.GetId().Equals(newStylist.GetId());
+        bool nameEquality = this.GetName().Equals(newStylist.GetName());
+        return (idEquality && nameEquality);
+      }
+    }
+
+      public void Save()
+      {
+        MySqlConnection conn = DB.Connection();
+        conn.Open();
+        var cmd = conn.CreateCommand() as MySqlCommand;
+        cmd.CommandText = @"INSERT INTO stylists (name) VALUES (@name);";
+        MySqlParameter name = new MySqlParameter();
+        name.ParameterName = "@name";
+        name.Value = this._name;
+        cmd.Parameters.Add(name);
+        cmd.ExecuteNonQuery();
+        _id = (int) cmd.LastInsertedId; // <-- This line is new!
+        conn.Close();
+        if (conn != null)
+        {
+          conn.Dispose();
+        }
+      }
+
+      public void DeleteCat(int stylistId)
+      {
+        MySqlConnection conn = DB.Connection();
+        conn.Open();
+        var cmd = conn.CreateCommand() as MySqlCommand;
+
+        Stylist selectedStylist = Stylist.Find(stylistId);
+        Dictionary<string, object> model = new Dictionary<string, object>();
+        // List<Item> stylistItems = selectedStylist.GetItems();
+        model.Add("stylist", selectedStylist);
+
+        // foreach (Item item in stylistItems)
+        // {
+        //   item.Delete();
+        // }
+
+        cmd.CommandText = @"DELETE FROM stylists WHERE id = @thisId;";
+        MySqlParameter thisId = new MySqlParameter();
+        thisId.ParameterName = "@thisId";
+        thisId.Value = _id;
+        cmd.Parameters.Add(thisId);
+        cmd.ExecuteNonQuery();
+
+        conn.Close();
+        if (conn != null)
+        {
+          conn.Dispose();
+        }
+      }
 
 
 
