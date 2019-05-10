f2eb680b963baa72201e602ae9396c6aa7a0223b
kaya <jepsonkaya@gmail.com>
2019-05-10 08:23:56 -0700

    add initial stylist structure, class and constructor

---
+++ HairSalon.Solutions/HairSalon/Models/Stylist.cs
@@ -0,0 +1,42 @@
+using System.Collections.Generic;
+using MySql.Data.MySqlClient;
+
+namespace HairSalon.Models
+{
+  public class Stylist
+  {
+    private string _name;
+    private int _id;
+
+    public Stylist(string stylistName, int id = 0)
+    {
+      _name = stylistName;
+      _id = id;
+    }
+
+    public string GetName()
+    {
+      return _name;
+    }
+
+    public int GetId()
+    {
+      return _id;
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  }
+}
